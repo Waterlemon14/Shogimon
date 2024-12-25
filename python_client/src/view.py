6c3dfa6a85534d19ff8319a19fbd5b68140f5fb6
HoweZae <jdtomanan@up.edu.ph>
2024-12-25 14:13:27 +0800

    feat: Add classes, method signatures

--- python_client/src/view.py
+++ python_client/src/view.py
@@ -1,5 +1,37 @@
 import pygame
 
+class Character:
+    def __init__(self):
+        ...
+
+    def render_to_tile(self):
+        """
+        Render to respective tile
+        """
+        ...
+
+class Tile:
+    def __init__(self, x: int, y: int):
+        ...
+
+    def mark_occupied(self, char: Character):
+        ...
+
+    def mark_unoccupied(self):
+        ...
+
+    def render_to_screen(self):
+        """
+        Render to screen -- get from GameView
+        """
+        ...
+
 class GameView:
     def __init__(self):
+        ...
+
+    def render_tile(self):
+        ...
+
+    def render_to_screen(self):
         ...
