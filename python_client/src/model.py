770474121bd437ebd537e990f57e804eb64109ab
HoweZae <jdtomanan@up.edu.ph>
2024-12-26 23:04:01 +0800

    fix: Add parameter to make_turn(); import Movement

--- python_client/src/model.py
+++ python_client/src/model.py
@@ -1,10 +1,11 @@
 import pygame
+from project_types import Movement
 
 class GameModel:
     def __init__(self):
         ...
 
-    def make_turn(self):
+    def make_turn(self, turn: Movement):
         ...
 
     def new_game(self):
