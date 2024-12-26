41f9db91736f4ba17897290168060d5c3678200b
HoweZae <jdtomanan@up.edu.ph>
2024-12-26 13:06:21 +0800

    feat: Method signatures

--- python_client/src/controller.py
+++ python_client/src/controller.py
@@ -6,5 +6,14 @@ class GameController:
         self._model = model
         self._view = view
 
+    def on_make_turn(self):
+        ...
+
+    def on_next_round(self):
+        self._model.next_round()
+
+    def on_new_game(self):
+        ...
+
     def start(self):
         self._view.run()
--- python_client/src/model.py
+++ python_client/src/model.py
@@ -3,3 +3,6 @@ import pygame
 class GameModel:
     def __init__(self):
         ...
+
+    def next_round(self):
+        ...
