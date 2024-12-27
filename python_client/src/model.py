28a1da8733ec4b59417ee699078d7468a89f4e8c
HoweZae <jdtomanan@up.edu.ph>
2024-12-27 15:26:45 +0800

    feat: Add get_movement_range

--- python_client/src/model.py
+++ python_client/src/model.py
@@ -4,15 +4,34 @@ from project_types import GameState, Movement, PieceKind
 from view import Tile
 
 class Piece:
-    def __init__(self, piece: PieceKind, tile: Tile):
-        self._piece = piece
+    def __init__(self, kind: PieceKind, tile: Tile):
+        self._kind = kind
         self._tile = tile
         self._is_captured = False
 
     def is_captured(self) -> bool:
         return self._is_captured
 
-    def possible_moves(self) -> list:
+    def get_movement_range(self) -> list[Movement]:
+        match self._kind:
+            case PieceKind.EEVEE:
+                ...
+
+            case PieceKind.PIKACHU:
+                ...
+
+            case PieceKind.TURTWIG:
+                ...
+
+            case PieceKind.SYLVEON:
+                ...
+
+            case default:
+                ...
+
+        return []
+
+    def possible_moves(self) -> list[Movement]:
         """
         Returns list of possible moves given board state and list from get_movement_range()
         """
@@ -26,7 +45,10 @@ class ProtectedPiece:
     def is_immobile(self) -> bool:
         return self.possible_moves == []
 
-    def possible_moves(self) -> list:
+    def get_movement_range(self) -> list[Movement]:
+        ...
+
+    def possible_moves(self) -> list[Movement]:
         ...
 
 class GameModel:
