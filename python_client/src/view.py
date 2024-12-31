21f4cae609dddcf6068452067ebdc9145b939dd6
HoweZae <jdtomanan@up.edu.ph>
2024-12-31 09:54:26 +0800

    feat: Add coords per initialization in init_state

--- python_client/src/view.py
+++ python_client/src/view.py
@@ -124,33 +124,29 @@ class RenderableBoard:
 
     def _init_state(self):
         '''
-        Need to replace ellipses, piece_id's
+        Need to replace piece_id's
         '''
         for i in range(BOARD_COLS):
-            self._location_to_tile[Location(6, i)].\
-                mark_occupied(LivePiece(PieceKind.EEVEE, -1, PlayerNumber.ONE, Location(6, i)))
-            self._location_to_tile[Location(1, i)].\
-                mark_occupied(LivePiece(PieceKind.EEVEE, -1, PlayerNumber.TWO, Location(1, i)))
+            self._location_to_tile[Location(6, i)]. \
+                mark_occupied(LivePiece(PieceKind.EEVEE, -1, PlayerNumber.ONE, Location(6,i)))
+            self._location_to_tile[Location(1, i)]. \
+                mark_occupied(LivePiece(PieceKind.EEVEE, -1, PlayerNumber.TWO, Location(1,i)))
 
-        self._location_to_tile[...].\
-            mark_occupied(LivePiece(PieceKind.PIKACHU, -1, PlayerNumber.ONE, ...))
-        self._location_to_tile[...].\
-            mark_occupied(LivePiece(PieceKind.PIKACHU, -1, PlayerNumber.TWO, ...))
-
-        self._location_to_tile[...].\
-            mark_occupied(LivePiece(PieceKind.TURTWIG, -1, PlayerNumber.ONE, ...))
-        self._location_to_tile[...].\
-            mark_occupied(LivePiece(PieceKind.TURTWIG, -1, PlayerNumber.TWO, ...))
-        
-        self._location_to_tile[...].\
-            mark_occupied(LivePiece(PieceKind.LATIOS, -1, PlayerNumber.ONE, ...))
-        self._location_to_tile[...].\
-            mark_occupied(LivePiece(PieceKind.LATIOS, -1, PlayerNumber.TWO, ...))
-        
-        self._location_to_tile[...].\
-            mark_occupied(LivePiece(PieceKind.LATIAS, -1, PlayerNumber.ONE, ...))
-        self._location_to_tile[...].\
-            mark_occupied(LivePiece(PieceKind.LATIAS, -1, PlayerNumber.TWO, ...))
+        for row, p_num in [(7, PlayerNumber.ONE), (0, PlayerNumber.TWO)]:
+            self._location_to_tile[Location(row, 1)]. \
+                mark_occupied(LivePiece(PieceKind.PIKACHU, -1, p_num, Location(row, 1)))
+            self._location_to_tile[Location(row, 6)]. \
+                mark_occupied(LivePiece(PieceKind.PIKACHU, -1, p_num, Location(row, 6)))
+            
+            self._location_to_tile[Location(row, 0)]. \
+                mark_occupied(LivePiece(PieceKind.TURTWIG, -1, p_num, Location(row, 7)))
+            self._location_to_tile[Location(row, 7)]. \
+                mark_occupied(LivePiece(PieceKind.TURTWIG, -1, p_num, Location(row, 7)))
+            
+            self._location_to_tile[Location(row, 3)]. \
+                mark_occupied(LivePiece(PieceKind.LATIOS, -1, p_num, Location(row, 3)))
+            self._location_to_tile[Location(row, 4)]. \
+                mark_occupied(LivePiece(PieceKind.LATIAS, -1, p_num, Location(row, 4)))
 
     def get_tile(self, location) -> Tile:
         return self._location_to_tile[location]
