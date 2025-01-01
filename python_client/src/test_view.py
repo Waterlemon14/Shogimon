35f8989bffd24f474a1e18728c2b82d27b92878c
Jose Tomanan <jdtomanan@up.edu.ph>
2025-01-01 15:20:42 +0800

    feat: Update test_view

--- python_client/src/test_view.py
+++ python_client/src/test_view.py
@@ -1,5 +1,7 @@
 from view import GameView
+from model import GameModel
 
 if __name__ == '__main__':
-    view = GameView()
+    model = GameModel.default()
+    view = GameView(model.state)
     view.run()
