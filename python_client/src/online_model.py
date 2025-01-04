2fc9f9dbe6840971dade9a74b97fe3c3818d42e0
HoweZae <jdtomanan@up.edu.ph>
2025-01-04 10:55:59 +0800

    feat: Add initial methods

--- python_client/src/online_model.py
+++ python_client/src/online_model.py
@@ -1,11 +1,14 @@
 from typing import Self
 
-from project_types import (
-    BOARD_ROWS, BOARD_COLS
-    GameState, Movement, PieceKind, Location, PlayerNumber, MovePossibilities, PiecePositions, LivePiece, PlayerAction, ActionType, GameStatus,
-    )
+from project_types import *
 from model import *
 
-def OnlineModel(GameModel):
-    ...
+class OnlineModel(GameModel):
+    def __init__(self, state: GameState, board: Board, player: PlayerNumber, action_count: int):
+        super().__init__(state, board, player, action_count)
+
+    def _update_state(self):
+        return super()._update_state()
+    
+    # ikaw na magtuloy brother
 
