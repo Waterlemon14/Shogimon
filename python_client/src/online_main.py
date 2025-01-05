ce8fad8c8f014a09064395cff9d4ecee2a8fb551
HoweZae <jdtomanan@up.edu.ph>
2025-01-05 15:33:23 +0800

    feat: Remove online_model

--- python_client/src/online_main.py
+++ python_client/src/online_main.py
@@ -1,11 +1,9 @@
-from online_model import OnlineModel
+from model import GameModel
 from online_view import OnlineView
 from controller import GameController
 
 if __name__ == '__main__':
-
-
-    model = OnlineModel.default()
+    model = GameModel.default()
     view = OnlineView(model.state)
 
     controller = GameController(model, view)
